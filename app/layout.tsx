60754a01fddd8dc0e6d7cc29000e98165588073e
Diego Diaz De La Concha <diegodiazdelaconcha93@gmail.com>
2025-05-29 10:30:54 -0600

    se agrego el favicon y nombre en el metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type React from "react"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { LanguageProvider } from "@/contexts/language-context"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Developer Portfolio",
-  description: "A professional portfolio showcasing development skills and projects"
+  title: "Av-Skallet",
+  description: "A professional Developer Business"
 }
 
 export default function RootLayout({
@@ -17,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/public/favicon.ico"/>
+      </Head>
       <body className={`${inter.className} bg-black text-white`}>
         <LanguageProvider>{children}</LanguageProvider>
       </body>
